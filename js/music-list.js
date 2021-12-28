22508adc5c1bed0a1332fc9a590f32ede35e5958
kmyg <90238429+kmyg@users.noreply.github.com>
2021-12-28 11:25:22 +0900

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Awake",
+    artist: "",
+    img: "Awake",
+    src: "Awake"
+  },
+  {
+    name: "daytime",
+    artist: "",
+    img: "daytime",
+    src: "daytime"
+  },
+  {
+    name: "break",
+    artist: "",
+    img: "break",
+    src: "break"
+  },
+  {
+    name: "YUGURE",
+    artist: "",
+    img: "dusk",
+    src: "dusk"
+  },
+  {
+    name: "chilly",
+    artist: "",
+    img: "chilly",
+    src: "chilly"
+  },
+  {
+    name: "Haunt",
+    artist: "",
+    img: "Haunt",
+    src: "Haunt"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
